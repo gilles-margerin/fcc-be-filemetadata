2e8961f97705d95d75ccfe8ff34d9f0ba5413753
Gilles Margerin <gillesmargerin@gmail.com>
2021-02-16 19:18:23 +0100

    path bugfix

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ connection.once("open", () => {
 const app = express();
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, './uploads')
+    cb(null, './uploads/')
   },
   filename: function(req, file, cb) {
     cb(null, dayjs().toString() + path.extname(file.originalname))
